f1a668b2213e2b4d1da2a487f4baa460d0186108
Julien Mathis <jmathis@centreon.com>
2010-07-22 17:08:31 +0000

    move into gmap directory in order to add an installer

---
+++ centreon-gmap/sql/uninstall.sql
@@ -0,0 +1,8 @@
+DELETE FROM `topology` WHERE `topology_page` = '9';
+DELETE FROM `topology` WHERE `topology_page` = '901';
+DELETE FROM `topology` WHERE `topology_page` = '90101';
+DELETE FROM `topology` WHERE `topology_page` = '9010101';
+DELETE FROM `topology` WHERE `topology_page` = '9010102';
+DELETE FROM `topology` WHERE `topology_page` = '5010190';
+DROP TABLE `gmap_op`;
+DROP TABLE `locations`;
