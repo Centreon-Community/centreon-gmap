97ce76cda7ccacb729c6898cb8cb138490c0c7c6
Julien Mathis <jmathis@centreon.com>
2010-07-23 12:31:57 +0000

    big clean : step2
    => todo : the same work for hostgroups

--- www/centreon-gmap/DB-Func.php
+++ www/centreon-gmap/DB-Func.php
@@ -8,67 +8,68 @@ Version: 1.1
 Designed for: Centreon v2
 For information : justin@ensgrp.com or www.ensgrp.com
 **************************************************/
-function createMapLocations($pearDB,$oreon,$location_array){
-	global $smarty_hg_dot,$smarty_host_dot;
-	$lat = $location_array[0];
-	$long = $location_array[1];
-	$h_id = $location_array[2];
-	$hg_id = $location_array[3];
-	$l_id = $location_array[4];
 	
-	if ($hg_id == 0) {
-		$DBRESULT =& $pearDB->query("SELECT host_id as id,host_name as name FROM host WHERE host_id='$h_id'");
-	}
-	if ($hg_id > 0)  {
-	$DBRESULT4 =& $pearDB->query("SELECT hg_id as id,hg_name as name FROM hostgroup WHERE hg_id='$hg_id'");
-        while($DBRESULT4->fetchInto($location_n)) {
-	$location_name = $location_n['name'];
-	}
-
-
-        	 $DBRESULT =& $pearDB->query("SELECT hostgroup_hg_id as id,host_name as name FROM hostgroup_relation JOIN host ON hostgroup_relation.host_host_id = host.host_id WHERE hostgroup_relation.hostgroup_hg_id='$hg_id'");
-		//$DBRESULT =& $pearDB->query("SELECT hg_id as id,hg_name as name FROM hostgroup WHERE hg_id='$hg_id'");
-		}  
-	      if (PEAR::isError($DBRESULT))
-                                print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
-                        $n = 0;
-                        while($DBRESULT->fetchInto($new_map_location)) {
-                                $i = 0;
-                                $location_nam = $new_map_location['name'];
+	function createMapLocations($pearDB,$oreon,$location_array){
+		global $smarty_hg_dot,$smarty_host_dot;
+		
+		$lat = $location_array[0];
+		$long = $location_array[1];
+		$h_id = $location_array[2];
+		$hg_id = $location_array[3];
+		$l_id = $location_array[4];
+	
+		if ($hg_id == 0) {
+			$DBRESULT =& $pearDB->query("SELECT host_id as id,host_name as name FROM host WHERE host_id='$h_id'");
+		}
+		if ($hg_id > 0)  {
+			$DBRESULT4 =& $pearDB->query("SELECT hg_id as id,hg_name as name FROM hostgroup WHERE hg_id='$hg_id'");
+        	while ($location_n =& $DBRESULT4->fetchRow()) {
+				$location_name = $location_n['name'];
+			}
+			
+			$DBRESULT =& $pearDB->query("SELECT hostgroup_hg_id as id,host_name as name FROM hostgroup_relation JOIN host ON hostgroup_relation.host_host_id = host.host_id WHERE hostgroup_relation.hostgroup_hg_id='$hg_id'");
+			//$DBRESULT =& $pearDB->query("SELECT hg_id as id,hg_name as name FROM hostgroup WHERE hg_id='$hg_id'");
+			}
+			if (PEAR::isError($DBRESULT)) {
+				print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";	
+			}
+	        
+	        $n = 0;
+	        while ($new_map_location =& $DBRESULT->fetchRow()) {
+	            $i = 0;
+	            $location_nam = $new_map_location['name'];
 				$location_id = $new_map_location['id'];
 				## First, set the location dot as green
 				$host_status = "<a href=main.php?p=2020202&host_name=".urlencode($location_name)."><img src=\"modules/gmap/img/green-dot.png\"></a>";
-		
+			
 				$n++;
-				if($n == 10) {
+				if ($n == 10) {
 					$host_status = $host_status."<br>";
 					$n = 0;
 				}
 				$smarty_host_dot[$l_id][$location_name] = $host_status;
 			}
+			
 			$n = 0;
 			$hg_status = "iconUp";
-			
-
+				
 			$smarty_hg_dot[$l_id][] = $location_nam;
 			$smarty_hg_dot[$l_id][] = $hg_status;
 			$smarty_hg_dot[$l_id][] = $lat;
 			$smarty_hg_dot[$l_id][] = $long;
 			$smarty_hg_dot[$l_id][] = $h_id;
 			$smarty_hg_dot[$l_id][] = $l_id;
-
-			
 		}  
-                $DBRESULT->free();
+        $DBRESULT->free();
 
-	        if (!isset($oreon)) {
-                exit();
-        }
+	    if (!isset($oreon)) {
+           exit();
+    }
 
 
 	function readConfigOptions()	{
-		global $pearDB;
-		global $form;
+		global $pearDB, $form;
+		
 		$gmap_cfg = array();
 		$res =& $pearDB->query("SELECT * FROM gmap_op WHERE id = '1' LIMIT 1");
 		if (PEAR::isError($pearDB)) {
@@ -80,8 +81,12 @@ function createMapLocations($pearDB,$oreon,$location_array){
 	}	
 	
 	function updateGmapCFG($id = null)	{
-		if (!$id) return;
 		global $form, $pearDB, $oreon;
+		
+		if (!$id) { 
+			return;
+		}
+		
 		$ret = array();
 		$ret = $form->getSubmitValues();
 		$rq = "UPDATE `gmap_op` SET ";
@@ -97,34 +102,31 @@ function createMapLocations($pearDB,$oreon,$location_array){
 		isset($ret["width"]) && $ret["width"] != NULL ? $rq .= "'".htmlentities($ret["width"], ENT_QUOTES)."' ": $rq .= "NULL ";
 		$rq .= "WHERE `id` = '".$id."'";
 		$DBRESULT =& $pearDB->query($rq);
-		if (PEAR::isError($DBRESULT))
+		if (PEAR::isError($DBRESULT)) {
 			print $DBRESULT->getDebugInfo()."<br>";
-
+		}
 	}
 
 
 
 	function getHostgroupLatLong2($hostOrGroup)  {
-		global $smarty_host_list;
-		global $smarty_host;
-                global $pearDB;
-                global $oreon;
-                $graphTs = array(NULL=>NULL);
+		global $smarty_host_list, $smarty_host, $pearDB, $oreon;
+		
+        $graphTs = array(NULL=>NULL);
 		if ($hostOrGroup == 'host') {
-                	$DBRESULT =& $pearDB->query("SELECT * FROM locations where hg_id IS NULL");
+        	$DBRESULT =& $pearDB->query("SELECT * FROM locations where hg_id IS NULL");
 		}
 		if ($hostOrGroup == 'group') {
-                        $DBRESULT =& $pearDB->query("SELECT * FROM locations where h_id IS NULL");
-                }
+        	$DBRESULT =& $pearDB->query("SELECT * FROM locations where h_id IS NULL");
+        }
 
-                if (PEAR::isError($DBRESULT))
-                        print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
-	   // fetch 	
-           while($DBRESULT->fetchInto($locations)) {
+        if (PEAR::isError($DBRESULT)) {
+    	    print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
+        }
+        while ($locations =& $DBRESULT->fetchRow()) {
 			if ($hostOrGroup == 'host') {
 				$host_name = $locations['h_id'];
-			}
-			else {
+			} else {
 				$host_name = $locations['hg_id'];
 			}
 			$smarty_host[$host_name][] = $locations['l_id'];
@@ -135,71 +137,60 @@ function createMapLocations($pearDB,$oreon,$location_array){
 			$smarty_host[$host_name][] = $locations['long'];
 			
 	    }
-                $DBRESULT->free();
-                $DBRESULT =& $pearDB->query("SELECT * FROM host WHERE host_register='1'");
-                if (PEAR::isError($DBRESULT))
-                        print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
-
-           while($DBRESULT->fetchInto($host)) {
-			
-                        $smarty_host_list[$host['host_id']] = $host['host_name'];
-
-	   }
-                $DBRESULT->free();
-
+        $DBRESULT->free();
+        $DBRESULT =& $pearDB->query("SELECT * FROM host WHERE host_register='1'");
+        if (PEAR::isError($DBRESULT)) {
+        	print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
         }
+        while ($host =& $DBRESULT->fetchRow()) {
+			$smarty_host_list[$host['host_id']] = $host['host_name'];
+	   	}
+       	$DBRESULT->free();
+	}
+	
 	function getHostGroupList() {
-	     	global $smarty_hostgroup_list;
-                global $pearDB;
-                global $oreon;
+		global $smarty_hostgroup_list, $pearDB, $oreon;
 
 		$DBRESULT =& $pearDB->query("SELECT * FROM hostgroup WHERE hg_activate='1'");
-                if (PEAR::isError($DBRESULT))
-                        print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
-
-           	while($DBRESULT->fetchInto($hostgroup)) {
-                        $smarty_hostgroup_list[$hostgroup['hg_id']] = $hostgroup['hg_name'];
-
-                }
-                $DBRESULT->free();
-	}
-        function getHostList() {
-                global $smarty_hostgroup_list;
-                global $pearDB;
-                global $oreon;
-
-                $DBRESULT =& $pearDB->query("SELECT * FROM host WHERE host_activate='1' AND host_register='1'");
-                if (PEAR::isError($DBRESULT))
-                        print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
-
-                while($DBRESULT->fetchInto($host)) {
-                        $smarty_hostgroup_list[$host['host_id']] = $host['host_name'];
-
-                }
-                $DBRESULT->free();
+        if (PEAR::isError($DBRESULT)) {
+                print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
         }
-
-function getValidLocations($pearDB,$oreon)  {
- 	$DBRESULT =& $pearDB->query("SELECT * FROM locations WHERE `lat` IS NOT NULL AND `long` IS NOT NULL");
-    if (PEAR::isError($DBRESULT))
-        print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
-    while($DBRESULT->fetchInto($coors)) {
-  		$l_id = $coors['l_id'];
-		$lat = $coors['lat'];	
-        	$long = $coors['long'];
-		$h_id = $coors['h_id'];
-		$hg_id = $coors['hg_id'];
-    		$locations[$l_id][] = $lat;
-    		$locations[$l_id][] = $long;
-    		$locations[$l_id][] = $h_id;
-    		$locations[$l_id][] = $hg_id;
-		$locations[$l_id][] = $l_id;
+	    while ($hostgroup =& $DBRESULT->fetchInto()) {
+        	$smarty_hostgroup_list[$hostgroup['hg_id']] = $hostgroup['hg_name'];
+        }
+        $DBRESULT->free();
 	}
-    ## return valid location array 
-    return $locations;
-}
+        
+	function getHostList() {
+        global $smarty_hostgroup_list, $pearDB, $oreon;
 
+        $DBRESULT =& $pearDB->query("SELECT * FROM host WHERE host_activate='1' AND host_register='1'");
+        if (PEAR::isError($DBRESULT)) {
+        	print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
+        }
+	    while ($host =& $DBRESULT->fetchRow()) {
+        	$smarty_hostgroup_list[$host['host_id']] = $host['host_name'];
+        }
+        $DBRESULT->free();
+	}
 
-
-
+	function getValidLocations($pearDB,$oreon)  {
+	 	$DBRESULT =& $pearDB->query("SELECT * FROM locations WHERE `lat` IS NOT NULL AND `long` IS NOT NULL");
+	    if (PEAR::isError($DBRESULT))
+	        print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
+	    while ($coors =& $DBRESULT->fetchRow()) {
+	  		$l_id = $coors['l_id'];
+			$lat = $coors['lat'];	
+	       	$long = $coors['long'];
+			$h_id = $coors['h_id'];
+			$hg_id = $coors['hg_id'];
+	    	$locations[$l_id][] = $lat;
+	    	$locations[$l_id][] = $long;
+	    	$locations[$l_id][] = $h_id;
+	    	$locations[$l_id][] = $hg_id;
+			$locations[$l_id][] = $l_id;
+		}
+	    ## return valid location array 
+	    return $locations;
+	}
 ?>
--- www/centreon-gmap/gmap_config_host.php
+++ www/centreon-gmap/gmap_config_host.php
@@ -9,8 +9,8 @@ Designed for: Centreon v2
 For information : justin@ensgrp.com or www.ensgrp.com
 **************************************************/
 
-	if (!isset ($oreon))
-		exit ();
+	if (!isset($oreon))
+		exit();
 
 	## Pear library
 	require_once "HTML/QuickForm.php";
@@ -26,46 +26,62 @@ For information : justin@ensgrp.com or www.ensgrp.com
 	## PHP functions
 	require_once "DB-Func.php";
 	require_once "./include/common/common-Func.php";
-        
-	## define globals
-	global $pearDB;
-	global $oreon;
 
-## legacy update host	
-if(isset($_GET['l_id'])) {
-	$l_id = $_GET['l_id'];
-	$hg_id = $_GET['hg_id'];
-	$lat = $_GET['lat'];
-	$long = $_GET['long'];
-	$address = $_GET['address'];
-	$action = $_GET['action'];	
-        if($action == "new") {
-		$DBRESULT =& $pearDB->query("INSERT into`locations` SET `h_id`='$hg_id',`lat`='$lat',`long`='$long',`address`='$address'");
-	}
- 	if($action == "update") {
-		$DBRESULT =& $pearDB->query("UPDATE `locations` SET `h_id`='$hg_id',`lat`='$lat',`long`='$long',`address`='$address' WHERE l_id='$l_id' LIMIT 1");
-	}
-	if($action == "delete") {
+	## legacy update host	
+	if (isset($_GET['l_id'])) {
+		$l_id = $_GET['l_id'];
+		$hg_id = $_GET['hg_id'];
+		$lat = $_GET['lat'];
+		$long = $_GET['long'];
+		$address = $_GET['address'];
+		$action = $_GET['action'];	
+        
+        if ($action == "new") {
+			$DBRESULT =& $pearDB->query("INSERT into`locations` SET `h_id`='$hg_id',`lat`='$lat',`long`='$long',`address`='$address'");
+		} else if ($action == "update") {
+			$DBRESULT =& $pearDB->query("UPDATE `locations` SET `h_id`='$hg_id',`lat`='$lat',`long`='$long',`address`='$address' WHERE l_id='$l_id' LIMIT 1");
+		} else if ($action == "delete") {
                 $DBRESULT =& $pearDB->query("DELETE from `locations` WHERE l_id='$l_id' LIMIT 1");
         }
 
-	if (PEAR::isError($DBRESULT))
-        	print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
-}
-// we pass either group or host
-getHostgroupLatLong2('host');
-
-getHostList();
-$gmap_op = readConfigOptions($pearDB,$oreon);
-	// if the gmap api is set, display page
-	if($gmap_op['api_key']) {
+		if (PEAR::isError($DBRESULT)) {
+      		print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
+		}
+	}
+	
+	/*
+	 * we pass either group or host
+	 */
+	getHostgroupLatLong2('host');
+	getHostList();
+	
+	$gmap_op = readConfigOptions($pearDB,$oreon);
+	
+	/*
+	 * if the gmap api is set, display page 
+	 */ 
+	if ($gmap_op['api_key']) {
 		$tpl = new Smarty();
-	    	$tpl = initSmartyTpl($path, $tpl);
-	    	$tpl->assign("h_names", $smarty_host);
+	   	$tpl = initSmartyTpl($path, $tpl);
+	   	$tpl->assign("h_names", $smarty_host);
 		$tpl->assign("host_list", $smarty_host_list);
 		$tpl->assign("hostgroup_list", $smarty_hostgroup_list);
 		$tpl->assign("gmap_key", $gmap_op['api_key']);
-	    	$tpl->display("gmap_config_host.ihtml");
+		$tpl->assign("gmap_lat", $gmap_op['lat']);
+        $tpl->assign("gmap_long", $gmap_op['long']);
+        $tpl->assign("gmap_height", $gmap_op['height']);
+        $tpl->assign("gmap_width", $gmap_op['width']);
+        
+        /*
+         * translations
+         */
+        $tpl->assign("host", _("Hosts"));
+        $tpl->assign("address", _("Address, postal code, city, country"));
+        $tpl->assign("latitude", _("Latitude"));
+        $tpl->assign("longitude", _("Longitude"));
+        $tpl->assign("actions", _("Actions"));
+        $tpl->assign("addLocation", _("Add a location for a host"));
+        
+	   	$tpl->display("gmap_config_host.ihtml");
 	}
-
 ?>
