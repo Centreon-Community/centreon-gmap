e78505f47c4c7ecf764d8f39f9379a0adf87538d
Julien Mathis <jmathis@centreon.com>
2010-07-23 09:33:41 +0000

    fix problem with api. Now ok

--- www/centreon-gmap/gmap.php
+++ www/centreon-gmap/gmap.php
@@ -12,41 +12,45 @@ For information : justin@ensgrp.com or www.ensgrp.com
 	if (!isset ($oreon))
 		exit ();
 
-	//Pear library
+	/*
+	 * Pear library
+	 */
 	require_once 'HTML/QuickForm.php';
 	require_once 'HTML/QuickForm/advmultiselect.php';
 	require_once 'HTML/QuickForm/Renderer/ArraySmarty.php';
 
-      $form = new HTML_QuickForm();
+    $form = new HTML_QuickForm();
 
-	//Path to the configuration dir
+	/*
+	 * Path to the configuration dir
+	 */
 	global $path;
 	$path = "./modules/centreon-gmap/";
 
-	#PHP functions
+	/*
+	 * PHP functions
+	 */
 	require_once "DB-Func.php";
 	require_once "./include/common/common-Func.php";
 
-        // define globals
-	global $pearDB;
-	global $oreon;
 
-        // We read the status.dat file.
+    /*
+     * We read the status.dat file.
+     */
 	$gmap_op = readConfigOptions($pearDB,$oreon);
 	
-	if($gmap_op['api_key']) {
+	if ($gmap_op['api_key']) {
 	    $tpl = new Smarty();
 	    $tpl = initSmartyTpl($path, $tpl);
 	    $tpl->assign("gmap_key", $gmap_op['api_key']);
-            $tpl->assign("gmap_lat", $gmap_op['lat']);
-            $tpl->assign("gmap_long", $gmap_op['long']);
-            $tpl->assign("gmap_height", $gmap_op['height']);
-            $tpl->assign("gmap_width", $gmap_op['width']);
+        $tpl->assign("gmap_lat", $gmap_op['lat']);
+        $tpl->assign("gmap_long", $gmap_op['long']);
+        $tpl->assign("gmap_height", $gmap_op['height']);
+        $tpl->assign("gmap_width", $gmap_op['width']);
 	    $tpl->display("gmap.ihtml");
-	}
-	else 
+	} else {
 	   echo "You need to obtain a valid google map key which can be obtained <a href=http://www.google.com/apis/maps/>Here</a><br>"
 		."After obtaining a valid google api key, add it to the module configuration under <a href=main.php?p=5010190&o=w>options->General Options->Gmap</a> ";
-
+	}
 
 ?>
