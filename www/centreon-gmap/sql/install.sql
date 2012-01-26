3b2f029200d6ebae1e8bab5a55494787a824af5d
breakintheweb <justin@ensgrp.com>
2012-01-26 11:58:03 +0000

    fixed sql install primary key issue

--- www/centreon-gmap/sql/install.sql
+++ www/centreon-gmap/sql/install.sql
@@ -20,7 +20,7 @@ CREATE TABLE locations (
 INSERT INTO `mod_gmap_options` (`id`, `lat`, `lng`, `height`, `zoomLevel`) VALUES (1, 47.9016, 1.99951, 500, 5);
 INSERT INTO `css_color_menu` (`id_css_color_menu`, `menu_nb`, `css_name`) VALUES (9, 9, 'green_css.php');
 INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES ('', 9, NULL, './include/common/javascript/changetab.js', 'initChangeTab'), ('', 901, NULL, './include/common/javascript/changetab.js', 'initChangeTab');
-NSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`, `topology_style_class`, `topology_style_id`, `topology_OnClick`, `readonly`) VALUES
+INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`, `topology_style_class`, `topology_style_id`, `topology_OnClick`, `readonly`) VALUES
 ('', 'View', './modules/centreon-gmap/img/ico_google.jpg', 7, 704, 100, 1, './modules/centreon-gmap/gmap.php', NULL, '0', '1', '1', NULL, NULL, NULL, '1'),
 ('', 'Host Config', './img/icones/16x16/server_network.gif', 7, 702, 101, 2, './modules/centreon-gmap/gmap_config_host.php', NULL, '0', '1', '1', NULL, NULL, NULL, '1'),
 ('', 'Host Group Config', './img/icones/16x16/clients.gif', 7, 703, 102, 1, './modules/centreon-gmap/gmap_config_hostGroup.php', NULL, '0', '1', '1', NULL, NULL, NULL, '1'),
